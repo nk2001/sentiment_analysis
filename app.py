7d56a898ddae933a8013c5b3a63e0b4e6a83f2d4
nk2001 <kadoor@yahoo.com>
2021-07-27 17:43:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 st.write("# Real Time Sentiment Analysis")
 
-user_input = st.text_input("Please Input a sentence: ")
-nltk.download("vader_lexicon")
-s = SentimentIntensityAnalyzer()
-score = s.polarity_scores(user_input)
-
-st.write( str(score["pos"]*100) +" Positive")
-st.write( str(score["neg"]*100) +" Negative")
-st.write( str(score["neu"]*100) +" Neutral")
+user_input = st.text_input("Please Input a sentence below:")
+if(st.button('Submit')):
+	nltk.download("vader_lexicon")
+	s = SentimentIntensityAnalyzer()
+	score = s.polarity_scores(user_input)
+	st.write("The sentence is analyzed as follows")
+	st.write( str(score["pos"]*100) + "% Positive")
+	st.write( str(score["neg"]*100) + "% Negative")
+	st.write( str(score["neu"]*100) + "% Neutral")
